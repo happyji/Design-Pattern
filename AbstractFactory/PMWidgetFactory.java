65a29fe57deb8b4cd15c0f03038dde9e0e7ada3b
prashant pandey <prashantsundu@gmail.com>
2016-06-10 16:32:39 +0530

    Add files via upload

---
+++ AbstractFactory/PMWidgetFactory.java
@@ -0,0 +1,13 @@
+package AbstractFactory;
+
+public class PMWidgetFactory implements WidgetFactory {
+    @Override
+    public ScrollBar createScrollBar() {
+        return new PMScrollBar();
+    }
+
+    @Override
+    public Window createWindow() {
+        return new PMWindow();
+    }
+}
---
+++ AbstractFactory/WidgetFactory.java
@@ -0,0 +1,6 @@
+package AbstractFactory;
+
+public interface WidgetFactory {
+    ScrollBar createScrollBar();
+    Window createWindow();
+}
